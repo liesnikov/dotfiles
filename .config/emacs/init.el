2c3150ee27c9819e7f61b939ca1994f3a7fbe705
Bohdan <b.liesnikov+git@gmail.com>
2022-03-08 21:10:06 +0100

    add M-g M-g binding to avy goto line

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -307,7 +307,8 @@
 ;; move around efficiently
 (use-package avy
   :bind (("C-;" . avy-goto-char-timer)
-         ("M-g g" . avy-goto-line))
+         ("M-g g" . avy-goto-line)
+         ("M-g M-g" . avy-goto-line))
   :config
   (avy-setup-default))
 
