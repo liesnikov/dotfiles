9aa09e837e6ed8ea4aa5efe4ea0d0b20871f910a
Bohdan <b.liesnikov+git@gmail.com>
2023-04-17 11:04:24 +0200

    silly indent

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -774,7 +774,7 @@
 ;;;#### agda
 ;; agda2 mode, gets appended by `agda-mode setup`
 (load-file (let ((coding-system-for-read 'utf-8))
-             (shell-command-to-string "agda-mode locate")))
+                (shell-command-to-string "agda-mode locate")))
 
 
 ;;; Commentary:
