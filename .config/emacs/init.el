4c5f2acd4896eb1b63d3ee593ba18f168e962b6a
liesnikov <17168103+liesnikov@users.noreply.github.com>
2025-10-29 13:23:15 +0100

    follow-up on ce4bf16
    orderless-annotation is not a style, but a style modifier
    
    enabling it by default makes vertico crash somehow
    but using & as the first letter when using autocompletion help

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1021,8 +1021,6 @@ When there is ongoing compilation, nothing happens."
   ;; regular expressions in any order.
   :custom
   (completion-styles '(orderless basic))
-  (completion-category-overrides
-      '((variable (styles orderless-annotation))))
   )
 
 (use-package consult
