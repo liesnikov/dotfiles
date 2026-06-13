26680af2c65ac1d3e4ca901063dd06a4b14e3202
liesnikov <17168103+liesnikov@users.noreply.github.com>
2026-06-13 18:23:06 +0200

    in consult config move variable changes from init to custom

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1079,12 +1079,28 @@ When there is ongoing compilation, nothing happens."
          )
 
   :custom
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (consult-narrow-key "<") ;; "C-+"
+
+  ;; Use Consult to select xref locations with preview
+  (xref-show-xrefs-function #'consult-xref)
+  (xref-show-definitions-function #'consult-xref)
+
+  ;; Optionally configure preview.
+  ;; The default value is 'any, such that any key triggers the preview.
+  (consult-preview-key (:debounce 0.4 any))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+
   ;; instead of which-key
   ;; following https://www.matem.unam.mx/~omar/apropos-emacs.html#the-case-against-which-key-a-polemic
   (prefix-help-command #'embark-prefix-help-command)
+
+  :hook
   ;; Enable automatic preview at point in the *Completions* buffer.
   ;; This is relevant when you use the default completion UI.
-  :hook (completion-list-mode-hook . consult-preview-at-point-mode)
+  (completion-list-mode-hook . consult-preview-at-point-mode)
 
   :functions
   consult-register-window
@@ -1106,31 +1122,15 @@ When there is ongoing compilation, nothing happens."
   (advice-add #'register-preview :override #'consult-register-window)
   (setq register-preview-delay 0.5)
 
-  ;; Use Consult to select xref locations with preview
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
-
   ;; Configure other variables and modes in the :config section,
   ;; after lazily loading the package.
   :config
 
-  ;; Optionally configure preview. The default value
-  ;; is 'any, such that any key triggers the preview.
-  ;; (setq consult-preview-key 'any)
-  ;; (setq consult-preview-key "M-.")
-  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
-  ;; For some commands and buffer sources it is useful to configure the
-  ;; :preview-key on a per-command basis using the `consult-customize' macro.
-  (consult-customize
-   consult-theme :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep consult-man
-   consult-bookmark consult-recent-file consult-xref
-   ;; :preview-key "M-."
-   :preview-key '(:debounce 0.4 any))
-
-  ;; Optionally configure the narrowing key.
-  ;; Both < and C-+ work reasonably well.
-  (setq consult-narrow-key "<") ;; "C-+"
+  ;(consult-customize
+  ; consult-theme :preview-key '(:debounce 0.5 any)
+  ; consult-ripgrep consult-git-grep consult-grep consult-man
+  ; consult-bookmark consult-recent-file consult-xref
+  ; :preview-key '(:debounce 0.5 any))
 
   ;; Optionally make narrowing help available in the minibuffer.
   ;; You may want to use `embark-prefix-help-command' or which-key instead.
