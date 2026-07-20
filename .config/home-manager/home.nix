5174431152d39dfdce3fa79826b42036005404d1
liesnikov <17168103+liesnikov@users.noreply.github.com>
2026-07-20 02:45:28 +0200

    switch to managing files with home-manager
    remove index.sh
    
    update vale mapping

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -12,7 +12,7 @@
 
       direnv
       nix-direnv # support direnv for flakes
-      nixfmt-rfc-style # format nix files
+      nixfmt # format nix files
       # nix-index # tool to find nixpkgs fields corresponding to paths
       nixd # language server for nix
 
@@ -71,6 +71,49 @@
 
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
+
+    home.file = let
+      link = config.lib.file.mkOutOfStoreSymlink;
+      dotfiles = "${config.home.homeDirectory}/dotfiles";
+    in {
+      ".bash_aliases".source = link "${dotfiles}/.bash_aliases";
+      ".bash_functions".source = link "${dotfiles}/.bash_functions";
+      ".bash_logout".source = link "${dotfiles}/.bash_logout";
+      ".bashrc".source = link "${dotfiles}/.bashrc";
+      ".profile".source = link "${dotfiles}/.profile";
+      ".local/bin/caps-unlock".source = link "${dotfiles}/.local/bin/caps-unlock";
+      ".local/bin/xfce-toggle-cycle-workspaces".source = link "${dotfiles}/.local/bin/xfce-toggle-cycle-workspaces";
+    };
+
+    xdg.configFile = let
+      link = config.lib.file.mkOutOfStoreSymlink;
+      dotfiles = "${config.home.homeDirectory}/dotfiles/.config";
+    in {
+      "alacritty/alacritty.toml".source = link "${dotfiles}/alacritty/alacritty.toml";
+      "emacs/.gitignore".source = link "${dotfiles}/emacs/.gitignore";
+      "emacs/custom.el".source = link "${dotfiles}/emacs/custom.el";
+      "emacs/early-init.el".source = link "${dotfiles}/emacs/early-init.el";
+      "emacs/init.el".source = link "${dotfiles}/emacs/init.el";
+      "emacs/yasnippets/markdown-mode/footnote".source = link "${dotfiles}/emacs/yasnippets/markdown-mode/footnote";
+      "emacs/yasnippets/markdown-mode/https".source = link "${dotfiles}/emacs/yasnippets/markdown-mode/https";
+      "ghostty/config.ghostty".source = link "${dotfiles}/ghostty/config.ghostty";
+      "gtk-3.0/gtk.css".source = link "${dotfiles}/gtk-3.0/gtk.css";
+      "nix/nix.conf".source = link "${dotfiles}/nix/nix.conf";
+      "python/pythonrc".source = link "${dotfiles}/python/pythonrc";
+      "tmux/tmux.conf".source = link "${dotfiles}/tmux/tmux.conf";
+      "vale/.vale.ini".source = link "${dotfiles}/vale/casual.ini";
+      "vale/academic.ini".source = link "${dotfiles}/vale/academic.ini";
+      "vale/casual.ini".source = link "${dotfiles}/vale/casual.ini";
+      "vale/config/vocabularies/type-theory/accept.txt".source = link "${dotfiles}/vale/config/vocabularies/type-theory/accept.txt";
+      "vale/config/vocabularies/type-theory/reject.txt".source = link "${dotfiles}/vale/config/vocabularies/type-theory/reject.txt";
+      "vim/ftplugin/c.vim".source = link "${dotfiles}/vim/ftplugin/c.vim";
+      "vim/ftplugin/cpp.vim".source = link "${dotfiles}/vim/ftplugin/cpp.vim";
+      "vim/ftplugin/cs.vim".source = link "${dotfiles}/vim/ftplugin/cs.vim";
+      "vim/ftplugin/haskell.vim".source = link "${dotfiles}/vim/ftplugin/haskell.vim";
+      "vim/ftplugin/python.vim".source = link "${dotfiles}/vim/ftplugin/python.vim";
+      "vim/vimrc".source = link "${dotfiles}/vim/vimrc";
+      "xdg-terminals.list".source = link "${dotfiles}/xdg-terminals.list";
+    };
 
     dconf.settings = {
       "org/gnome/Console" = {
